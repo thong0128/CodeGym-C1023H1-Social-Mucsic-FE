9bda7f53cc6e1b191a6ff83a32280c91d486ec51
tuan anh <tuananhtran.xd@gmail.com>
2024-04-17 10:03:54 +0700

    form register

--- src/apis/index.js
+++
@@ -1,4 +0,0 @@
-
-export * from './home'
-export * from './music'
-
---
+++ src/register/Register.js
@@ -0,0 +1,41 @@
+import './register.css';
+export default function Register() {
+
+    return (
+        <>
+            <div className="main">
+                <h2>Register</h2>
+                <form>
+                    <div className="row mb-3">
+                        <label htmlFor="userName" className="col-sm-2 col-form-label">User name</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="userName"/>
+                        </div>
+                    </div>
+                    <div className="row mb-3">
+                        <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
+                        <div className="col-sm-10">
+                            <input type="password" className="form-control" id="inputPassword"/>
+                        </div>
+                    </div>
+                    <div className="row mb-3">
+                        <label htmlFor="confirmPassword" className="col-sm-2 col-form-label">Confirm Password</label>
+                        <div className="col-sm-10">
+                            <input type="password" className="form-control" id="confirmPassword"/>
+                        </div>
+                    </div>
+                    <div className="row mb-3">
+                        <label htmlFor="phoneNumber" className="col-sm-2 col-form-label">Phone number</label>
+                        <div className="col-sm-10">
+                            <input type="password" className="form-control" id="phoneNumber"/>
+                        </div>
+                    </div>
+
+
+                    <button type="submit" className="btn btn-primary">Sign in</button>
+                </form>
+            </div>
+        </>
+    );
+
+}
