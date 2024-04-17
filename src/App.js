1b3b29090e54049dfbed2c676539612488c95dfd
tuan anh <tuananhtran.xd@gmail.com>
2024-04-17 16:24:52 +0700

    login done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Register from "./register/Register";
 import {ToastContainer} from "react-toastify";
 import {AppProvider} from "./Context/AppContext";
 import Login from "./login/Login";
+import Home from "./home/Home";
 
 
 
@@ -16,6 +17,7 @@ function App() {
               <Routes>
                       <Route path={"login"} element={<Login/>}></Route>
                       <Route path={"register"} element={<Register/>}></Route>
+                      <Route path={"home"} element={<Home/>}></Route>
               </Routes>
               </AppProvider> <ToastContainer />
 
