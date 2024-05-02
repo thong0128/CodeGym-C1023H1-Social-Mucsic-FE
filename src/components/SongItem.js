e7d25b2849c23700ec882ee6dfc2cdbac52eaa17
viethungle1 <hunglv0801@gmail.com>
2024-05-02 14:29:15 +0700

    count listen music

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -27,42 +27,51 @@ const SongItem = ({thumbnail, title, artists, sid, author, countLikes, releaseDa
         })
 
     }
+
+    const handleCount = ()=>{
+        axios.put(`http://localhost:8080/songs/count/${sid}`).then((res) => {
+            console.log("success")
+        })
+    }
     return (
         <div className="col-md-4 song-item">
-            <div
-                onClick={() => {
-                    console.log("sid:", sid)
-                    dispatch(findSongById(sid))
-                }}
-                className={'group flex p-3 rounded-md hover:bg-main-200 hover:border border-gray-200'}>
-                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md">
-                    <img
-                        onClick={() => {
-                            navigate("/detailSong/" + sid)
-                        }}
-                        src={thumbnail} alt='' className="h-full w-full object-cover object-center"/>
-                </div>
-                <div className={'ml-4 flex flex-1 flex-col'}>
-                    <div>
-                        <div className="flex justify-between text-base font-medium">
-                            <h3>
-                                <Link to={`/detailSong/${sid}`}
-                                      className="text-slate-900 group-hover:text-black font-semibold">{title}</Link>
-                            </h3>
-                        </div>
-                        <p className="mb-2 text-slate-500 group-hover:text-black text-sm">{artists}</p>
-                        <p className="mb-2 text-slate-500 group-hover:text-black text-sm">{author}</p>
+            <div onClick={handleCount}>
+                <div
+                    onClick={() => {
+                        console.log("sid:", sid)
+                        dispatch(findSongById(sid))
+                    }}
+                    className={'group flex p-3 rounded-md hover:bg-main-200 hover:border border-gray-200'}>
+                    <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md">
+                        <img
+                            onClick={() => {
+                                navigate("/detailSong/" + sid)
+                            }}
+                            src={thumbnail} alt='' className="h-full w-full object-cover object-center"/>
                     </div>
-                </div>
-                <div className={'flex flex-col'} onClick={handleLike}>
-                    {checkLike ? <IoHeartSharp size={24}/> : <IoHeartOutline size={24}/>}
-                </div>
-                <div className={'flex flex-col mx-2'}>
-                    <span>{countLikes}</span>
-                </div>
-                <div className="flex">
-                    <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500"><Dropdown_song
-                        idSong={sid}/></button>
+                    <div className={'ml-4 flex flex-1 flex-col'}>
+                        <div>
+                            <div className="flex justify-between text-base font-medium">
+                                <h3>
+                                    <Link to={`/detailSong/${sid}`}
+                                          className="text-slate-900 group-hover:text-black font-semibold">{title}</Link>
+                                </h3>
+                            </div>
+                            <p className="mb-2 text-slate-500 group-hover:text-black text-sm">{artists}</p>
+                            <p className="mb-2 text-slate-500 group-hover:text-black text-sm">{author}</p>
+                        </div>
+                    </div>
+                    <div className={'flex flex-col'} onClick={handleLike}>
+                        {checkLike ? <IoHeartSharp size={24}/> : <IoHeartOutline size={24}/>}
+                    </div>
+                    <div className={'flex flex-col mx-2'}>
+                        <span>{countLikes}</span>
+                    </div>
+                    <div className="flex">
+                        <button type="button" className="font-medium text-indigo-600 hover:text-indigo-500">
+                            <Dropdown_song
+                                idSong={sid}/></button>
+                    </div>
                 </div>
             </div>
         </div>
